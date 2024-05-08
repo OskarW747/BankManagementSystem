7643ba01a434e0760778625759795af148500fa0
OskarW747 <oskagn@outlook.com>
2024-05-08 16:57:17 +0200

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+// Menusindex
+#define MAIN_MENU       0
+#define DEFAULT_MENU    0
+
+int mode = DEFAULT_MENU;
+
+void initializeProgram() {
+    cout << "Bankmanagement System" << endl;
+}
+
+void menu(int& index) {
+    switch (index) {
+        case (MAIN_MENU):
+            cout << "1) Withdraw" << endl;
+            cout << "2) Deposit" << endl;
+            cin.get();
+            break;
+    }
+}
+
+int main()
+{   
+    initializeProgram();
+
+    /* Main loop */
+    while (true)
+    {
+        menu(mode);
+    }
+}
