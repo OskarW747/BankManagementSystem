bfe88ff81aadc30d419b6ed6771905154b7952c8
OskarW747 <oskagn@outlook.com>
2024-05-20 17:26:09 +0200

    .

--- src/Account.cpp
+++ src/Account.cpp
@@ -2,16 +2,22 @@
 #include <iostream>
 #include <fstream>
 
-void Account::withdraw(int _amount) {
+void Account::withdraw(int _amount)
+{
 
     balance = balance - _amount;
-
 }
 
-void Account::deposit(int _amount){
+void Account::deposit(int _amount)
+{
     balance = balance + _amount;
 }
 
-//void Account::save(ofstream _saveFile){
+void Account::save()
+{
     
+}
+
+// void Account::save(ofstream _saveFile){
+
 //}
