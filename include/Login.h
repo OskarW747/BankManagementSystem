5c20ff2a1138b977536900f2f7e9a8fe61816369
OskarW747 <oskagn@outlook.com>
2024-05-27 22:36:46 +0200

    login page

---
+++ include/Login.h
@@ -0,0 +1,13 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+
+
+class LoginPage {
+    public:
+        void print();
+
+    private:
+
+};
+
+#endif
---
+++ src/Login.cpp
@@ -0,0 +1,11 @@
+#include "Login.h"
+#include <iostream>
+
+using namespace std;
+
+void LoginPage::print()
+{
+    cout << "-----------------------------" << endl;
+    cout << "         Login Page" << endl;
+    cout << "-----------------------------" << endl;
+}
